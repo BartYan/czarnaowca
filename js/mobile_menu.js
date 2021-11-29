1d0386202d41461c5b1a3ed1531d93402ea48d0e
Bartosz Janusz <bartosz.yan@gmail.com>
2021-11-29 19:22:10 +0100

    first commit

---
+++ js/mobile_menu.js
@@ -0,0 +1,27 @@
+//hamburger and overlay variables
+let hamburger = document.querySelector('.nav_hamburger');
+let mobileMenu = document.getElementById("mobileMenu");
+let overlayLink = document.querySelectorAll('.nav_overlay-content--link');
+
+//Hamburger overlay functions
+let active = false;
+
+const overlay = () => {
+
+  if (active === false) {
+    mobileMenu.style.display = "block";
+    active = true;
+  } else {
+    mobileMenu.style.display = "none";
+    active = false;
+  }
+};
+
+overlayLink.forEach(item => {
+  item.addEventListener('click', event => {
+    mobileMenu.style.display = "none";
+    active = false;
+  })
+});
+
+hamburger.addEventListener("click", overlay);
